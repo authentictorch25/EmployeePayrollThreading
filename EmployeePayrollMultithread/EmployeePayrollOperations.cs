7fda9264dc9f403bed1065bfa1c20ccd57ccf780
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-16 03:19:56 +0530

    [Akash] Added program to include N logger

--- EmployeePayrollMultithread/EmployeePayrollOperations.cs
+++ EmployeePayrollMultithread/EmployeePayrollOperations.cs
@@ -9,22 +9,30 @@ using System.Threading.Tasks;
 namespace EmployeePayrollMultithread
 {
    public class EmployeePayrollOperations
-        { 
-         public static SqlConnection connection { get; set; }
+        {
+        public static SqlConnection connection { get; set; }
+
+        NLog nLog = new NLog();
+
+        /// <summary>
+        /// Mutex is mutual exclusion used to synchronise the threads
+        /// Similar to locks
+        /// </summary>
         private static Mutex mutex = new Mutex();
 
         /// <summary>
-        /// Adds the employee list to data base.
+        /// UC 1 : Adds the employee list to data base.
         /// </summary>
         /// <param name="employeeList">The employee list.</param>
-        /// <returns></returns>
         public bool AddEmployeeListToDataBase(List<EmployeeModel> employeeList)
         {
             foreach (var employee in employeeList)
             {
+                nLog.LogDebug($"Adding the Employee: {employee.EmployeeName} via ThreadID: {Thread.CurrentThread.ManagedThreadId}");
                 Console.WriteLine("Employee being added:" + employee.EmployeeName);
                 bool flag = AddEmployeeToDatabase(employee);
                 Console.WriteLine("Employee added:" + employee.EmployeeName);
+                nLog.LogInfo($"Employee {employee.EmployeeName} added in Database via ThreadId: " + Thread.CurrentThread.ManagedThreadId);
                 if (flag == false)
                     return false;
             }
@@ -32,75 +40,31 @@ namespace EmployeePayrollMultithread
         }
 
         /// <summary>
-        /// Adds the employee list to database using thread.
+        /// UC 2 : Adds the employee list to database using thread.
         /// </summary>
         /// <param name="employeeList">The employee list.</param>
         public void AddEmployeeListToDataBaseWithThread(List<EmployeeModel> employeeList)
         {
             employeeList.ForEach(employeeData =>
             {
+                nLog.LogDebug($"Adding the Employee: {employeeData.EmployeeName} without synchronization via ThreadID: {Thread.CurrentThread.ManagedThreadId}");
                 //For each employeeData present in list new thread is created and all threads run according
                 //to the time slot assigned by the thread scheduler
-                Task thread = new Task(() =>
+                Thread thread = new Thread(() =>
                 {
                     Console.WriteLine("Current thread id: " + Thread.CurrentThread.ManagedThreadId);
                     Console.WriteLine("Employee Being added" + employeeData.EmployeeName);
-                    this.AddEmployeeToDatabase(employeeData);
+                    AddEmployeeToDatabase(employeeData);
                     Console.WriteLine("Employee added:" + employeeData.EmployeeName);
+                    nLog.LogInfo($"Employee {employeeData.EmployeeName} added in Database without synchronization via ThreadId: " + Thread.CurrentThread.ManagedThreadId);
                 });
                 thread.Start();
             });
+            Console.WriteLine("Total added employees: " + employeeList.Count);
         }
 
         /// <summary>
-        /// Adds the employee to database.
-        /// </summary>
-        /// <param name="employee">The employee.</param>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
-        public bool AddEmployeeToDatabase(EmployeeModel employee)
-        {
-            DBConnection dbc = new DBConnection();
-            connection = dbc.GetConnection();
-            try
-            {
-                using (connection)
-                {
-                    SqlCommand command = new SqlCommand("dbo.SpAddEmployeeDetails", connection);
-                    command.CommandType = CommandType.StoredProcedure;
-                    command.Parameters.AddWithValue("@name", employee.EmployeeName);
-                    command.Parameters.AddWithValue("@start", employee.StartDate);
-                    command.Parameters.AddWithValue("@gender", employee.Gender);
-                    command.Parameters.AddWithValue("@phone_number", employee.PhoneNumber);
-                    command.Parameters.AddWithValue("@address", employee.Address);
-                    command.Parameters.AddWithValue("@department", employee.Department);
-                    command.Parameters.AddWithValue("@Basic_Pay", employee.BasicPay);
-                    command.Parameters.AddWithValue("@Deductions", employee.Deductions);
-                    command.Parameters.AddWithValue("@Taxable_pay", employee.TaxablePay);
-                    command.Parameters.AddWithValue("@Income_tax", employee.Tax);
-                    command.Parameters.AddWithValue("@Net_pay", employee.NetPay);
-                    connection.Open();
-                    var result = command.ExecuteNonQuery();
-                    connection.Close();
-                    if (result != 0)
-                    {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                if (connection.State.Equals("Open"))
-                    connection.Close();
-            }
-        }
-        /// <summary>
-        /// Adds the employee list to data base with synchronization.
+        /// UC 3 : Adds the employee list to data base with synchronization.
         /// </summary>
         /// <param name="employeeList">The employee list.</param>
         public void AddEmployeeListToDataBaseWithSynchronization(List<EmployeeModel> employeeList)
@@ -114,16 +78,70 @@ namespace EmployeePayrollMultithread
                     lock (employeeData)
                     {
                         //mutex.WaitOne();
+                        nLog.LogDebug($"Adding the Employee: {employeeData.EmployeeName} using synchronization via ThreadID: {Thread.CurrentThread.ManagedThreadId}");
                         Console.WriteLine("Current thread id: " + Thread.CurrentThread.ManagedThreadId);
                         Console.WriteLine("Employee Being added" + employeeData.EmployeeName);
                         this.AddEmployeeToDatabase(employeeData);
                         Console.WriteLine("Employee added:" + employeeData.EmployeeName);
+                        nLog.LogInfo($"Employee {employeeData.EmployeeName} added in Database using synchronization via ThreadId: " + Thread.CurrentThread.ManagedThreadId);
                         //mutex.ReleaseMutex();
                     }
                 });
                 thread.Start();
                 thread.Wait();
             });
+        }
+
+        /// <summary>
+        /// Adds the employee to database.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public bool AddEmployeeToDatabase(EmployeeModel employee)
+        {
+            lock (this)
+            {
+                DBConnection dbc = new DBConnection();
+                connection = dbc.GetConnection();
+                try
+                {
+                    using (connection)
+                    {
+                        SqlCommand command = new SqlCommand("dbo.SpAddEmployeeDetails", connection);
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@name", employee.EmployeeName);
+                        command.Parameters.AddWithValue("@start", employee.StartDate);
+                        command.Parameters.AddWithValue("@gender", employee.Gender);
+                        command.Parameters.AddWithValue("@phone_number", employee.PhoneNumber);
+                        command.Parameters.AddWithValue("@address", employee.Address);
+                        command.Parameters.AddWithValue("@department", employee.Department);
+                        command.Parameters.AddWithValue("@Basic_Pay", employee.BasicPay);
+                        command.Parameters.AddWithValue("@Deductions", employee.Deductions);
+                        command.Parameters.AddWithValue("@Taxable_pay", employee.TaxablePay);
+                        command.Parameters.AddWithValue("@Income_tax", employee.Tax);
+                        command.Parameters.AddWithValue("@Net_pay", employee.NetPay);
+                        connection.Open();
+                        var result = command.ExecuteNonQuery();
+                        connection.Close();
+                        if (result != 0)
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    if (connection.State.Equals("Open"))
+                        connection.Close();
+                }
+            }
+
         }
     }
 }
---
+++ EmployeePayrollMultithread/NLog.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollMultithread
+{
+    public class NLog
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Method to log for the debug operation
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+        /// <summary>
+        /// Method to log for the message at error level
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+        /// <summary>
+        /// Method to log for the message at information level
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+        /// <summary>
+        /// Method to log for the message at warning level
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
+    
